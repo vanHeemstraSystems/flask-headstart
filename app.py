ad8a5a171715d1894530b42aee656410afc0a56d
Willem van Heemstra <wvanheemstra@icloud.com>
2020-08-12 14:22:37 +0000

    Add methods to default route in app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ class Todo(db.Model):
     def __repr__(self):
         return '<Task %r>' % self.id
 
-@app.route('/')
+@app.route('/', methods=['POST','GET'])
 def index():
     # return "Hello, World!"
     return render_template('index.html')
